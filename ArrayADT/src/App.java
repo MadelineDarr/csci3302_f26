fe5bf412d45f6c4390c4756ffd3c6d422d5bcf60
MadelineDarr <darrmk@jacks.sfasu.edu>
2026-09-10 09:43:48 -0500

    Introduce MyArrayListADT interface
    Added MyArrayListADT interface with basic methods for array list operations.

--- ArrayADT/src/App.java
+++ ArrayADT/src/App.java
@@ -1,3 +1,14 @@
+public interface MyArrayListADT {
+
+    boolean isEmpty(); // returns true if the arrayList is empty
+    int size(); // returns number of elements in the arrayList
+    boolean contains(Object o);
+    void add(Student s);
+    Student remove(int index);
+}    
+
+
+
 public class App {
     
 }
